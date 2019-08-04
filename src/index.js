eaf088faaef0e54e8f44ce687c444bdad7f9144a
liangunjianpan <songsong@mail.com>
2019-08-04 17:16:53 +0800

    项目的环境搭建-路由-SCSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 //  入口文件
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import 'antd-mobile/dist/antd-mobile.css'
+import './index.scss'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
